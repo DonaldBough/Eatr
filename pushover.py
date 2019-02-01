e10eb2e0b25606f5fc050ec1068aa1e5221eed61
Kyle Disandro <kdisandro@kdisandro-ltm.internal.salesforce.com>
2019-02-01 18:17:06 -0500

    changing to subscription method

--- pushover.py
+++ pushover.py
@@ -4,7 +4,7 @@ conn = http.client.HTTPSConnection("api.pushover.net:443")
 conn.request("POST", "/1/messages.json",
   urllib.parse.urlencode({
     "token": "a6wz72iuhi513rs66muxezw7xow56p",
-    "user": "uvdqzng418uxukr15mc3eedbe8her7",
+    "user": "gig7rn39w69sf5ahk5uucvc3814m4c",
     "message": "Eat some protein yo",
   }), { "Content-type": "application/x-www-form-urlencoded" })
 conn.getresponse()
